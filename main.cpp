9a1880d029fe84a0e143f3226f97f0ef4037871d
vitaly <50383391+Neikow@users.noreply.github.com>
2025-11-20 11:08:24 +0100

    Merge Matrices branch

--- main.cpp
+++ main.cpp
@@ -216,12 +216,10 @@ std::vector<std::vector<float> > laplace_beltrami_matrix(Mesh &mesh) {
     const auto n_vertices = mesh.n_vertices();
     L.reserve(n_vertices);
 
-    for (int i = 0; i < n_vertices; ++i)
-    {
+    for (int i = 0; i < n_vertices; ++i) {
         L.emplace_back();
         L[i].reserve(n_vertices);
-        for (int j = 0; j < n_vertices; ++j)
-        {
+        for (int j = 0; j < n_vertices; ++j) {
             L[i].emplace_back(0.0f);
         }
     }
@@ -230,12 +228,11 @@ std::vector<std::vector<float> > laplace_beltrami_matrix(Mesh &mesh) {
         const int i = vv_it_i->idx();
         const auto vi = mesh.vertex_handle(i);
         float diag_sum = 0.0f;
-        for (Mesh::VertexVertexIter vv_it_j = mesh.vv_iter(vi); vv_it_j.is_valid(); ++vv_it_j)
-        {
+        for (Mesh::VertexVertexIter vv_it_j = mesh.vv_iter(vi); vv_it_j.is_valid(); ++vv_it_j) {
             const int j = vv_it_j->idx();
             const auto vj = mesh.vertex_handle(j);
 
-            auto [aij, bij] = cotan_ab_at_vertex(mesh, vi, vj);
+            auto [aij, bij] = compute_cotan_weight_for_vertices(mesh, vi, vj);
 
             L[i][j] = -(aij + bij) / (2 * A(mesh, vi));
             diag_sum -= (L[i][j]);
@@ -247,12 +244,12 @@ std::vector<std::vector<float> > laplace_beltrami_matrix(Mesh &mesh) {
     return L;
 }
 
-std::vector<std::vector<float>> laplace_beltrami_matrix_squared(Mesh &mesh) {
+std::vector<std::vector<float> > laplace_beltrami_matrix_squared(Mesh &mesh) {
     auto L = laplace_beltrami_matrix(mesh);
     const auto n = L.size();
 
     // Initialiser la matrice résultat L² avec des zéros
-    std::vector<std::vector<float>> L2(n, std::vector<float>(n, 0.0f));
+    std::vector<std::vector<float> > L2(n, std::vector<float>(n, 0.0f));
 
     // Multiplication matricielle L × L
     for (int i = 0; i < n; ++i) {
@@ -286,45 +283,6 @@ void cotangential_iterative_smoothing(Mesh &mesh, const float lambda) {
     }
 }
 
-std::vector<std::vector<float> > laplace_beltrami_matrix(Mesh &mesh) {
-    const auto n_vertices = mesh.n_vertices();
-    std::vector<std::vector<float> > M(n_vertices, std::vector<float>(n_vertices, 0.0f));
-
-    // Compute M matrix
-    for (auto vh: mesh.vertices()) {
-        int i = vh.idx();
-
-        float w = 0.0f;
-
-        for (auto vv_it = mesh.vv_iter(vh); vv_it.is_valid(); ++vv_it) {
-            int j = vv_it->idx();
-
-            auto [aij, bij] = compute_cotan_weight_for_vertices(mesh, vh, *vv_it);
-
-            M[i][j] = aij + bij;
-            w += aij + bij;
-        }
-
-        M[i][i] = -w;
-    }
-
-    std::vector<float> diagonal_D(n_vertices, 0.0f);
-
-    // Compute D matrix
-    for (int i = 0; i < n_vertices; ++i) {
-        diagonal_D[i] = 1.0f / (2.0f * A(mesh, mesh.vertex_handle(i)));
-    }
-
-    // Matrix Multiplication L = D * M
-    for (int i = 0; i < n_vertices; ++i) {
-        for (int j = 0; j < n_vertices; ++j) {
-            M[i][j] = diagonal_D[i] * M[i][j];
-        }
-    }
-
-    return M;
-}
-
 // Simple matrix multiplication for square matrices
 std::vector<std::vector<float> > mat_mul(
     const std::vector<std::vector<float> > &A,
@@ -344,28 +302,11 @@ std::vector<std::vector<float> > mat_mul(
     return C;
 }
 
-void laplace_beltrami_matrix_iterative_smoothing(Mesh &mesh, const float lambda) {
-    const auto L = laplace_beltrami_matrix(mesh);
-    const auto L2 = mat_mul(L, L);
-
-    // Update vertex positions
-    for (auto vh: mesh.vertices()) {
-        int i = vh.idx();
-        auto vi = mesh.point(vh);
-        OpenMesh::Vec3f delta = {0.0f, 0.0f, 0.0f};
-        for (int j = 0; j < mesh.n_vertices(); ++j) {
-            auto vj = mesh.point(mesh.vertex_handle(j));
-            delta += L2[i][j] * (vj - vi);
-        }
-        auto new_vi = vi + lambda * delta;
-        mesh.set_point(vh, new_vi);
-    }
-}
-
 enum Algorithm {
     UNIFORM_LAPLACE_BELTRAMI,
     COTANGENTIAL_LAPLACE_BELTRAMI,
-    LAPLACE_BELTRAMI_MATRIX
+    LAPLACE_BELTRAMI_MATRIX,
+    LAPLACE_BELTRAMI_MATRIX_BIS
 };
 
 std::string algorithm_to_string(const Algorithm algo) {
@@ -376,6 +317,8 @@ std::string algorithm_to_string(const Algorithm algo) {
             return "cotangential_laplace_beltrami";
         case LAPLACE_BELTRAMI_MATRIX:
             return "laplace_beltrami_matrix";
+        case LAPLACE_BELTRAMI_MATRIX_BIS:
+            return "laplace_beltrami_matrix_bis";
     }
     return "unknown";
 }
@@ -399,25 +342,24 @@ std::string get_output_file_name(
     return "outputs/" + base_name + "_" + algorithm_to_string(algo) + "_iter_" + std::to_string(iterations) + ".obj";
 }
 
-void square_matrix_iterative_smoothing(Mesh &mesh, const float lambda)
-{
+void square_matrix_iterative_smoothing(Mesh &mesh, const float lambda) {
     // Variante optimiser local : pas de construction de L ni L² explicite
     // L²*v[i] = L(L*v)[i] = Σ_{j voisin i} w_ij ( (L*v)[j] - (L*v)[i] )
 
     const auto n = mesh.n_vertices();
-    std::vector<OpenMesh::Vec3f> lap1(n, OpenMesh::Vec3f(0,0,0));
+    std::vector<OpenMesh::Vec3f> lap1(n, OpenMesh::Vec3f(0, 0, 0));
 
     // Premier passage : Laplacien cotangent
     for (Mesh::VertexIter v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
         const int i = v_it->idx();
         const auto vhi = mesh.vertex_handle(i);
         const auto vi = mesh.point(vhi);
-        OpenMesh::Vec3f acc(0,0,0);
+        OpenMesh::Vec3f acc(0, 0, 0);
         for (Mesh::VertexVertexIter vv_it = mesh.vv_iter(vhi); vv_it.is_valid(); ++vv_it) {
             const int j = vv_it->idx();
             const auto vhj = mesh.vertex_handle(j);
             const auto vj = mesh.point(vhj);
-            auto [aij, bij] = cotan_ab_at_vertex(mesh, vhi, vhj);
+            auto [aij, bij] = compute_cotan_weight_for_vertices(mesh, vhi, vhj);
             float w = -(aij + bij) / (2 * A(mesh, vhi)); // w_ij
             acc += w * (vj - vi);
         }
@@ -425,15 +367,15 @@ void square_matrix_iterative_smoothing(Mesh &mesh, const float lambda)
     }
 
     // Second passage : appliquer à lap1 pour approximer L²*v
-    std::vector<OpenMesh::Vec3f> lap2(n, OpenMesh::Vec3f(0,0,0));
+    std::vector<OpenMesh::Vec3f> lap2(n, OpenMesh::Vec3f(0, 0, 0));
     for (Mesh::VertexIter v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
         const int i = v_it->idx();
         const auto vhi = mesh.vertex_handle(i);
-        OpenMesh::Vec3f acc(0,0,0);
+        OpenMesh::Vec3f acc(0, 0, 0);
         for (Mesh::VertexVertexIter vv_it = mesh.vv_iter(vhi); vv_it.is_valid(); ++vv_it) {
             const int j = vv_it->idx();
             const auto vhj = mesh.vertex_handle(j);
-            auto [aij, bij] = cotan_ab_at_vertex(mesh, vhi, vhj);
+            auto [aij, bij] = compute_cotan_weight_for_vertices(mesh, vhi, vhj);
             float w = -(aij + bij) / (2 * A(mesh, vhi));
             acc += w * (lap1[j] - lap1[i]);
         }
@@ -451,15 +393,13 @@ void square_matrix_iterative_smoothing(Mesh &mesh, const float lambda)
 
 // Variante "bis" : on calcule la matrice L puis L² explicitement
 // Assez long et gourmand en mémoire.
-void square_matrix_iterative_smoothing_bis(Mesh &mesh, const float lambda)
-{
+void square_matrix_iterative_smoothing_bis(Mesh &mesh, const float lambda) {
     auto L2 = laplace_beltrami_matrix_squared(mesh); // construit L puis L^2
     const auto n = mesh.n_vertices();
 
     std::vector<OpenMesh::Vec3f> new_positions(n);
 
-    for (Mesh::VertexIter vv_it = mesh.vertices_begin(); vv_it != mesh.vertices_end(); ++vv_it)
-    {
+    for (Mesh::VertexIter vv_it = mesh.vertices_begin(); vv_it != mesh.vertices_end(); ++vv_it) {
         const int i = vv_it->idx();
         const auto vh = mesh.vertex_handle(i);
         const auto vi = mesh.point(vh);
@@ -514,8 +454,10 @@ int main(const int argc, char *argv[]) {
                 cotangential_iterative_smoothing(copy, lambda);
                 break;
             case LAPLACE_BELTRAMI_MATRIX:
-                laplace_beltrami_matrix_iterative_smoothing(copy, lambda);
+                square_matrix_iterative_smoothing(copy, lambda);
                 break;
+            case LAPLACE_BELTRAMI_MATRIX_BIS:
+                square_matrix_iterative_smoothing_bis(copy, lambda);
         }
     };
 
